69a49ca818b40e360cbbbdf685fe548d0f5a51e8
Vyacheslav Kirichenko <maker1992@mail.ru>
2020-10-31 16:00:43 +0300

    Добавил миграцию для инициализации схемы БД.

---
+++ Notes/Migrations/20201031113251_InitialModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Notes.Migrations
+{
+    public partial class InitialModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(maxLength: 50, nullable: false),
+                    IsCompleted = table.Column<bool>(nullable: false),
+                    IsImportant = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
