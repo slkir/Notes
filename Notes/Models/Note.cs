f622332a8a773e745e9fa9e3c8240d256e05087f
Vyacheslav Kirichenko <maker1992@mail.ru>
2020-10-31 13:33:40 +0300

    Add project files.

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Text { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsImportant { get; set; }
+    }
+}
---
+++ Notes/Models/NoteDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Notes.Models
+{
+    public class NoteDbContext : DbContext
+    {
+        public NoteDbContext(DbContextOptions<NoteDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
