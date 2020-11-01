75beaefda9363ce1dfb783fa175cf32880af3b6e
Vyacheslav Kirichenko <maker1992@mail.ru>
2020-11-01 18:01:09 +0300

    Добавил загрузку и удаление на сервере.

--- Notes/wwwroot/js/script.js
+++ Notes/wwwroot/js/script.js
@@ -1,53 +1,50 @@
 let noteList = [];
+let api = 'https://localhost:44346/api/notes/';
 
-function сreateTask(id, isChecked, text, isImportant) {
-    return {
-        id,
-        isChecked,
-        text,
-        isImportant
-    };
-}
+loadNotesFromServer(api);
 
-// Temp solution while server doesn't exist.
-function createRandomId(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
- }
- 
-function addTask() {
-
-    console.log('addTask() function');
-
+function addTask() {    
     if ($('#task').val() == '') {
         return;
     }
+   
+    let note = {
+        text: `${$('#task').val()}`,
+        isCompleted: false,
+        isImportant: false
+    }
 
-    // Add task to array.
-    let task = сreateTask(createRandomId(5), false, `${$('#task').val()}`, false);
-    noteList.push(task);
+    // Add task to server.
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json' 
+        },
+        type: "POST",
+        url: api,
+        data: JSON.stringify(note),
+        dataType: 'json',
+        success: function (data, status, xhr) {
+            let createdNoteUrl = xhr.getResponseHeader('Location');
 
-    // Add task to html.
-    $('.todo_list').append(`
-    <li id='${task.id}' class="task_item">
-        <i class="far fa-star"></i>
-        <input class="task_item__checkbox" type="checkbox" unchecked>${task.text}<i class="far fa-times-circle btn_del"></i>
-    </li>
-    `);
+            $.ajax({
+                url: createdNoteUrl,
+                success: function (data) {
+                    note.id = data.id;
+                    noteList.push(note);
 
-    $('#task').val('');
+                    addNoteMarkup(note);
+                    
+                    $('#task').val('');
+                    $(`li#${note.id}`).fadeOut(0);
+                    $(`li#${note.id}`).fadeIn(350);
 
-    $(`li#${task.id}`).fadeOut(0);
-    $(`li#${task.id}`).fadeIn(350);
-
-    console.log(noteList);
+                    console.log(noteList);
+                }
+            });
+        }
+    });   
 }
-
 
 function deleteTask(id){
     let index = noteList.findIndex((x) =>{
@@ -59,6 +56,12 @@ function deleteTask(id){
         this.remove();                
         console.log(noteList);
     })  
+
+    $.ajax({
+        type: "DELETE",
+        url: api + id,
+        success: function () { console.log('Deleted from server') },        
+    });
 }
 
 // Обработчики событий.
@@ -103,9 +106,6 @@ $('#task').on('keydown', function (arg) {
     }
 });
 
-
-loadNotesFromServer('https://localhost:44346/api/notes');
-
 function loadNotesFromServer(url) {
     loadedTaskList = [];
     $.ajax({
@@ -118,11 +118,20 @@ function loadNotesFromServer(url) {
 
 function loadTasks() {
     noteList.forEach((note) => {
-        $('.todo_list').append(`
+        addNoteMarkup(note)
+    })
+
+    $(`.todo_list li`).fadeOut(0);
+    $(`.todo_list li`).fadeIn(350);
+}
+
+function addNoteMarkup(note) {
+    $('.todo_list').append(
+        `
         <li id='${note.id}' class="task_item">
             <i class="${note.isImportant ? 'fas' : 'far'} fa-star"></i>
             <input class="task_item__checkbox" type="checkbox" ${note.isCompleted ? 'checked' : 'unchecked'}>${note.text}<i class="far fa-times-circle btn_del"></i>
         </li>
-        `);
-    })
+        `
+    );
 }
